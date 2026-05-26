6cd5c2e8aa13c47e57f7ac8fa3ee43b7be1b12db
dSleepy <dsleepy666@gmail.com>
2026-05-26 14:28:15 -0500

    Add dspic_cristal.h header file

---
+++ dspic_cristal.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   dspic_cristal.h
+ * Author: franc
+ *
+ * Created on April 1, 2026, 9:04 PM
+ */
+
+#ifndef DSPIC_CRISTAL_H
+#define FOSC 8000000UL
+#define FCY FOSC/2
+#include <libpic30.h>
+#define	DSPIC_CRISTAL_H
+
+#endif	/* DSPIC_CRISTAL_H */
