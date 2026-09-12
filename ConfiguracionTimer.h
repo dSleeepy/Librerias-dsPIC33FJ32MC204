0c016e9e7d044767f99408baa93f20268e7d687b
dSleepy <dsleepy666@gmail.com>
2026-09-11 21:48:03 -0500

    Timer Header
    Encabezado de la Configuración del Timer.

---
+++ ConfiguracionTimer.h
@@ -0,0 +1,13 @@
+#ifndef CONFIGURACIONTIMER_H
+#define	CONFIGURACIONTIMER_H
+
+void Timer1_Init_Temp(void);
+void Timer1_Init_Cont(void);
+void Timer2_Init_Temp(void);
+void Timer2_Init_Cont(void);
+void Timer3_Init_Temp(void);
+void Timer3_Init_Cont(void);
+void Timer32bits_Init(void);
+
+#endif	/* CONFIGURACIONTIMER_H */
+
